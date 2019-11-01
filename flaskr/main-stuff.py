2dec19ea7f089594b1e3c8315d3239734683a617
Thomas E <yeem@memeboi>
2019-10-31 19:16:19 -0700

    new directory layout

--- flaskr/main-stuff.py
+++ flaskr/main-stuff.py
@@ -10,18 +10,21 @@ we can do that on Apache2 more efficiently.
 
 FLASK DOCUMENTATION IS AT https://flask.palletsprojects.com/en/1.1.x/ YOU'LL WANT IT
 
-Oh and Leo, there's some info on configuring git for vscode in the google doc if you want that.
-actually never mind you should just go to
+oh and to set up vscode to work with the github repo you should you use the guide at
 https://stackoverflow.com/questions/55671825/how-to-use-%D0%B0-private-repository-with-vscode
 you'll find everything you need there
-
+except for the fact that you have to commit, then push manually (or at least I do)
 This file was pushed by Thomas btw
 """
 
 
 #import all the flask stuff we'll be needing
-from flask import Flask, escape, request, render_template # etc...
+from flask import Flask, escape, request, render_template, send_file, send_from_directory # etc...
 
 
 #initialize flask as "app". this matters not just for literally everything but for the WSGI as well
 app = Flask(__name__)
+
+@app.route('/')
+def showHomepage():
+    return send_file ()
