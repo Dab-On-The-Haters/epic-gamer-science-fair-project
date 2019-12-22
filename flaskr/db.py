a392a990ef1116a68bd60ef6c1a664e1e7042529
Thomas E <einstein.thomas2@gmail.com>
2019-12-22 11:55:36 -0800

    localhost is 127 not 172 lol

--- flaskr/db.py
+++ flaskr/db.py
@@ -3,7 +3,7 @@ import json
 with open("/home/thomas/.private-stuff.json") as f:
   psw = json.load(f)["FAIRY_PASSWORD"]
 
-conn = mariadb.connect(host="172.0.0.1", user="FAIRY", password=psw, database="SCIENCE_FAIR")
+conn = mariadb.connect(host="127.0.0.1", user="FAIRY", password=psw, database="SCIENCE_FAIR")
 cur = mariadb.cursor(dictionary=True) # maybe prepared=True in the future?
 
 
