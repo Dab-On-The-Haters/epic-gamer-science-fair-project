0bd699fc082ab6544211b62c216e317c1385e9fa
Thomas E <einstein.thomas2@gmail.com>
2019-12-27 16:35:27 -0800

    buffered cursor

--- flaskr/db.py
+++ flaskr/db.py
@@ -4,7 +4,7 @@ with open("/home/thomas/.private-stuff.json") as f:
   psw = json.load(f)["FAIRY_PASSWORD"]
 
 conn = mariadb.connect(host="127.0.0.1", user="FAIRY", password=psw, database="SCIENCE_FAIR")
-cur = conn.cursor(dictionary=True) # maybe prepared=True in the future?
+cur = conn.cursor(dictionary=True, buffered=True) # maybe prepared=True in the future?
 
 
 # to be exec. in files running this module conn.close()
