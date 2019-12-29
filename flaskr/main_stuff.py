c9a750e4c528642f522b0698f52aa894fa8603b0
Thomas E <einstein.thomas2@gmail.com>
2019-12-29 10:05:14 -0800

    bug fix for importing validators

--- flaskr/main_stuff.py
+++ flaskr/main_stuff.py
@@ -195,7 +195,7 @@ def datasetEditor():
         newEntry.select.label = FN
         newEntry.select.id = FN
         newEntry.id = FN
-        newEntry.select.validators = [v.DataRequired(message='Please select a column to go into the dataset.')]
+        newEntry.select.validators = [f.v.DataRequired(message='Please select a column to go into the dataset.')]
 
         choices = []
         for choice in columnInquiries[FN]:
