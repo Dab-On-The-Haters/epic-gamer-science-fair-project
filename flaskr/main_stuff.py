f21e2d7c794b638f5ee6cbff2f20ca071eb83e30
Thomas E <einstein.thomas2@gmail.com>
2020-01-01 18:41:02 -0800

    dumb bug fix

--- flaskr/main_stuff.py
+++ flaskr/main_stuff.py
@@ -276,7 +276,7 @@ def showProgress(ID):
 @app.route('/epoch-progress/<int:ID>')
 def epochProgress(ID):
     db.cur.execute('SELECT epoch FROM logs WHERE modelID = %s ORDER BY time_saved DESC LIMIT 1;', (ID,))
-    if db.cur.rowcount():
+    if db.cur.rowcount:
         return jsonify(db.cur.fetchone()['epoch'])
     else:  return jsonify(0)
 
