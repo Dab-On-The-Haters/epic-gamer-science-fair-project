822fe5f84804dbc4074e8c9b3d6368ce99429f99
Thomas E <einstein.thomas2@gmail.com>
2020-01-09 16:05:41 -0800

    typo fix

--- flaskr/main_stuff.py
+++ flaskr/main_stuff.py
@@ -495,7 +495,7 @@ def surveyRequest(user):
     reqBody = '''Hi {},
     please fill out a quick survey for Joe at https://99.199.44.233/survey. Your feedback is essential to keep on improving our service.'''
     reqHTML= ''' Hi {},<br>
-    please fill out a quick survey for Joe <a href="https://99.199.44.233/survey">right here<\a>. Your feedback is essential to keep on improving our service.'''
+    please fill out a quick survey for Joe <a href="https://99.199.44.233/survey">right here</a>. Your feedback is essential to keep on improving our service.'''
     
     surveyReq = Message(recipients=[user.email], body=reqBody.format(firstName), html=reqHTML.format(firstName), subject='Please give us some feedback on how to improve our site!', sender='joethernn@gmail.com')
     mail.send(surveyReq)
