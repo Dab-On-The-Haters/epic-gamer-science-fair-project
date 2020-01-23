97086f5336dc8972710a19c93e06e8ca6339296a
Thomas E <einstein.thomas2@gmail.com>
2020-01-23 09:17:51 -0800

    it's beautiful

--- survey-results.py
+++ survey-results.py
@@ -2,11 +2,10 @@
 import flaskr.db as db
 import csv
 
-db.cur.execute('DESC survey;')
-fieldnames = [field['Field'] for field in db.cur.fetchall()]
 
 with open('/home/thomas/Documents/survey-results.csv', 'w', newline='') as f:
-    csvf = csv.DictWriter(f, fieldnames=fieldnames)
+    db.cur.execute('DESC survey;')
+    csvf = csv.DictWriter(f, fieldnames=[field['Field'] for field in db.cur.fetchall()])
     csvf.writeheader()
 
     db.cur.execute('SELECT * FROM survey;')
