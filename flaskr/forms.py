18524d18a77db3a79657442805ccd2de7b05598d
Thomas E <einstein.thomas2@gmail.com>
2020-01-26 09:21:55 -0800

    stop requiring urls before they're removed

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -79,14 +79,14 @@ class loginForm(FlaskForm):
     username = f.StringField('Username', [v.InputRequired(r('username'))])
     password = f.PasswordField('Password', [v.InputRequired(r('password')), checkLoginValidity])
 
-urlm = 'Please enter a valid URL'
+urlm = 
 class datasetForm(FlaskForm):
     title = f.StringField('Name of this dataset', [v.InputRequired(r('dataset name')), v.length(5, 250, 'Dataset title must be between 5 and 250 characters long')])
     description = f.TextAreaField('Dataset description', [v.length(max=65500, message='Description can not be longer than 65,500 characters.')])
     files = f.FieldList(f.FileField('Custom dataset file from your computer'), max_entries=100)
     newFile = f.SubmitField('Add a new dataset file from your computer', render_kw=w3Button)
     removeFile = f.SubmitField('Remove the last dataset file entry', render_kw=w3Button)
-    URLs = f.FieldList(f5.URLField('URL of dataset of file', [v.InputRequired(urlm), v.URL(urlm)]), max_entries=100)
+    URLs = f.FieldList(f5.URLField('URL of dataset of file', [v.InputRequired(v.URL('Please enter a valid URL')]), max_entries=100)
     newURL = f.SubmitField('Add a new dataset URL', render_kw=w3Button)
     removeURL = f.SubmitField('Remove the last URL entry', render_kw=w3Button)
     uploadDataset = f.SubmitField('Upload the dataset', render_kw=w3Button)
