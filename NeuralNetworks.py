2e6608f7654168f254a7b1d5bd8d045c6009cbcf
Dab-On-The-Haters <57232776+Dab-On-The-Haters@users.noreply.github.com>
2020-06-17 21:17:50 -0700

    Add files via upload

---
+++ NeuralNetworks.py
@@ -0,0 +1,13 @@
+import numpy as np
+inputs = [1, 2, 3, 2.5]
+
+
+'''
+layer_outputs = []
+for neuronweights, neuron_bias in zip(weights, biases):
+    neuron_output = 0
+    for n_input, weight in zip(inputs, neuronweights):
+        neuron_output += n_input*weight
+        neuron_output += neuron_bias
+    layer_outputs.append(neuron_output)
+print(layer_outputs)'''
